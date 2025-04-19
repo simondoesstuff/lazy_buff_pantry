6e441ab039dbe5881b19268f20bae7caee3a3847
Simon Walker <simon@simonwalker.tech>
2025-04-18 18:50:57 -0600

    install.sh gens config.json

--- install.sh
+++ install.sh
@@ -26,6 +26,25 @@ if [ "$opts" == "u" ]; then
   exit 0
 fi
 
+if [ ! -f "$localDir/config.json" ]; then
+  echo "config.json set up..."
+  echo -n " - username: " && read username
+  echo -n " - password: " && read password
+  echo -n " - day (eg tues): " && read day
+  echo -n " - hour (eg 13): " && read hour
+  echo -n " - phone number (eg +19998887777): " && read phone
+  o="$localDir/config.json"
+  echo "{" > "$o"
+  echo "\"username\": \"$username\"," >> "$o"
+  echo "\"password\": \"$password\"," >> "$o"
+  echo "\"target\": {" >> "$o"
+  echo "\"day\": \"$day\"," >> "$o"
+  echo "\"hour\": $hour" >> "$o"
+  echo "}," >> "$o"
+  echo "\"phone_number\": \"$phone\"" >> "$o"
+  echo "}" >> "$o"
+fi
+
 ln -s "$localDir/$plist.plist" "$plist.plist"
 launchctl load -w "$plist.plist"
 echo "installed"
